5b84a9633de6d07546a9b29ec1d6e2d99a5de1df
0xSarkar <0xsarkar@gmail.com>
2023-06-13 15:55:41 +0530

    user message send function minimal

--- src/renderer.js
+++ src/renderer.js
@@ -2,10 +2,25 @@ import './index.css';
 
 import Alpine from 'alpinejs'
  
-window.Alpine = Alpine
 Alpine.start()
 
-function sendMsg() {
-  console.log("Woo!")
+const userMsgElem = window.document.getElementById("user-msg-txtarea")
+const chatAreaElem = window.document.getElementById("chat-area")
+
+function userMsgTemplate (userMsg) {
+  return `
+  <div class="self-end bg-blue-600 shadow text-white rounded-xl px-3 max-w-[80%] w-fit mb-4">
+    <p>
+      ${userMsg}
+    </p>
+  </div>`
 }
+
+function sendMsg() {
+  chatAreaElem.innerHTML = chatAreaElem.innerHTML + userMsgTemplate(userMsgElem.value)
+  userMsgElem.value = ""
+}
+
+window.Alpine = Alpine
+window.sendMsg = sendMsg;
 
