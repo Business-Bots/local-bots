b445552c47996f7f538e2a2bc4da77e4b3c357f9
0xSarkar <0xsarkar@gmail.com>
2023-06-13 16:05:24 +0530

    chat area ui updates

--- src/renderer.js
+++ src/renderer.js
@@ -7,7 +7,7 @@ Alpine.start()
 const userMsgElem = window.document.getElementById("user-msg-txtarea")
 const chatAreaElem = window.document.getElementById("chat-area")
 
-function userMsgTemplate (userMsg) {
+function userMsgTemplate(userMsg) {
   return `
   <div class="self-end bg-blue-600 shadow text-white rounded-xl px-3 max-w-[80%] w-fit mb-4">
     <p>
@@ -16,9 +16,21 @@ function userMsgTemplate (userMsg) {
   </div>`
 }
 
+function botMsgTemplate(botMsg) {
+  return `
+  <div class="border border-violet-100 bg-violet-50 shadow text-gray-900 rounded-xl px-3 max-w-[80%] w-fit">
+    <p>
+      ${botMsg}
+    </p>
+  </div>`
+}
+
 function sendMsg() {
   chatAreaElem.innerHTML = chatAreaElem.innerHTML + userMsgTemplate(userMsgElem.value)
+  chatAreaElem.parentElement.scrollTop = chatAreaElem.parentElement.scrollHeight;
   userMsgElem.value = ""
+  chatAreaElem.innerHTML = chatAreaElem.innerHTML + botMsgTemplate("Fake response from bot.")
+  chatAreaElem.parentElement.scrollTop = chatAreaElem.parentElement.scrollHeight;
 }
 
 window.Alpine = Alpine
